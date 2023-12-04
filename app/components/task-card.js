9ad402bb7e79ec4d996cebde0401429e2d97ec67
PensivePasta <pensivepasta@gmail.com>
2023-12-04 17:55:37 +0000

    Update task card with truncateText function on titles.

--- app/components/task-card.js
+++ app/components/task-card.js
@@ -9,6 +9,13 @@ const TaskCard = ({ task, onToggleCompletion, onEditTask }) => {
     onToggleCompletion(task.id);
   };
 
+  const truncateText = (text, limit) => {
+    if (text.length > limit) {
+      return text.substring(0, limit) + '...';
+    }
+    return text;
+  };
+
   return (
     <div className="bg-gray-50 p-4 h-32 rounded-3xl shadow-md mb-2.5 flex justify-between items-center md:min-w-[700px]">
       <div className="flex items-center space-x-4">
@@ -16,7 +23,7 @@ const TaskCard = ({ task, onToggleCompletion, onEditTask }) => {
           checked={enabled}
           onChange={handleToggle}
           className={`${enabled ? "bg-blue-500" : "bg-gray-200"}
-                relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none`}
+                relative inline-flex items-center h-6 rounded-full min-w-11 w-11 transition-colors focus:outline-none`}
         >
           <span
             className={`${enabled ? "translate-x-6" : "translate-x-1"}
@@ -24,7 +31,12 @@ const TaskCard = ({ task, onToggleCompletion, onEditTask }) => {
           />
         </Switch>
         <div>
-          <h3 className="text-xl font-semibold text-black">{task.title}</h3>
+          <h3 className="text-xl font-semibold text-black">
+            
+            {truncateText(task.title, 15)}
+            
+            </h3>
+          <p className="text-xs text-blue-500">View More</p>
           <div className="flex items-center">
             <p className="font-light text-gray-500 text-xs">{task.date}</p>
             <span className=" text-blue-500 text-sm px-2 py-1 ml-2">
