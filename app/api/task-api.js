a07240fc69eb918b71fe42bcdc01e34cb4d88fb7
PensivePasta <pensivepasta@gmail.com>
2023-12-05 19:22:59 +0000

    Move fetch tasks and add create and edit new tasks

---
+++ app/api/task-api.js
@@ -0,0 +1,80 @@
+const BASE_URL = "http://localhost:3001/api";
+
+export const fetchTasks = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/tasks`);
+    return await response.json();
+  } catch (error) {
+    console.error("fetchTasks error:", error);
+    throw error;
+  }
+};
+
+export const toggleTaskCompletion = async (taskId, completed) => {
+  try {
+    const response = await fetch(`${BASE_URL}/tasks/${taskId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completed }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to update task completion status.");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("toggleTaskCompletion error:", error);
+    throw error;
+  }
+};
+
+export const createNewTask = async (taskData) => {
+  try {
+    const response = await fetch(`${BASE_URL}/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskData),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to create new task.");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("createNewTask error:", error);
+    throw error;
+  }
+};
+
+export const updateTask = async (taskId, updatedTask) => {
+  try {
+    const response = await fetch(`${BASE_URL}/tasks/${taskId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to update task.");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("updateTask error:", error);
+    throw error;
+  }
+};
+
+export const deleteTask = async (taskId) => {
+  try {
+    const response = await fetch(`${BASE_URL}/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("deleteTask error:", error);
+    throw error;
+  }
+};
